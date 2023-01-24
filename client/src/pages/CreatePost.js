5e7768a4c191007723abeaf1363c3f5198d674c4
domains18 <gibsonsgibson88@gmail.com>
2023-01-24 21:25:55 +0300

    Ui layout

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
-import preview from '../assets/';
-import { getRandomPropmt } from '../utils';
+import { preview } from '../assets/';
+import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 const CreatePost = () => {
   return (
