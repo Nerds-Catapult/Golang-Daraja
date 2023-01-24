bf905b0bf0cbe5658876d52d1a75cff03cb60e5b
domains18 <gibsonsgibson88@gmail.com>
2023-01-24 20:25:04 +0300

    UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,8 +18,11 @@ export default function App() {
         </Link>
         <Link to="/create-post" className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"> Create</Link>
       </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9faf] min-h-[calc(100vh-73px)]">
-
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+        </Routes>
       </main>
     </BrowserRouter>
   )
