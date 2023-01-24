b872b9d0c3e69ac80a213a3bbec9f85fa50d93cc
domains18 <gibsonsgibson88@gmail.com>
2023-01-24 20:02:21 +0300

    router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { default as logo } from "./assets/logo.svg";
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
