48e039e3398e6fd29c7776aec084b2bd9ef2e856
domains18 <gibsonsgibson88@gmail.com>
2023-01-24 21:00:05 +0300

    UI layout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,11 @@ import { Loader, Cards, FormField } from '../components';
 const RenderCards = ({ data, title }) => {
   if(data?.length > 0 ) return data.map((post) => <Cards key={post._id} {...post} />)
 }
+return (
+  <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>
+    {title}
+  </h2>
+)
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -37,7 +42,7 @@ const Home = () => {
               </h2>
               )}
               <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
-
+                
               </div>
           </>
         )}
