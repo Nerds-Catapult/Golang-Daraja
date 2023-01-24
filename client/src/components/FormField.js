ca29137cbdc1a18c3648dc9519704b6900da217e
domains18 <gibsonsgibson88@gmail.com>
2023-01-24 22:11:35 +0300

    StateLogic

--- client/src/components/FormField.js
+++ client/src/components/FormField.js
@@ -15,8 +15,19 @@ const FormField = ({ LabelName, type, name, placeholder, value, handleChange, is
             className="text-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black"
           >
             Surprise Me
-          </button>)}
+          </button>
+          )}
       </div>
+      <input
+        type={type}
+        name={name}
+        id={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border-black text-gray-900 text-sm rounded-lg focus:ring-[#4649ff] focus:border-[#4649ff] outline-none block w-full p-3'
+      />
     </div>
   )
 }
