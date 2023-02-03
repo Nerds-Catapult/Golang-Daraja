35ba676209e0235a3521a07d81c771ce843be559
Domains18 <gibsonsgibson88@gmail.com>
2023-02-03 20:54:47 +0300

    StateLogic

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -4,13 +4,16 @@ import { preview } from '../assets/';
 import { getRandomPrompt } from '../utils';
 import { FormField, Loader } from '../components';
 const CreatePost = () => {
+  const generateImage = () => {
+
+  }
   const handleSubmit = (e) => {
-    
+
   };
   const handleChange = (e) => {
-    
+
   }
-  const handleSurpriseMe = () => { 
+  const handleSurpriseMe = () => {
 
   }
   const navigate = useNavigate();
@@ -19,7 +22,7 @@ const CreatePost = () => {
     prompt: '',
     image: '',
   });
-  const [generateImg, setGenerateImg] = React.useState(false);
+  const [generatingImg, setGeneratingImg] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   return (
     <section className='max-w-7xl mx-auto'>
@@ -50,15 +53,31 @@ const CreatePost = () => {
             name='prompt'
             handleChange={handleChange}
             isSurpriseMe
-            handleSurpriseMe = {handleSurpriseMe}
+            handleSurpriseMe={handleSurpriseMe}
           />
           <div className="relative bg-gray-50 border borser-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
             {form.photo ? (
               <img src={form.photo}
                 alt={form.prompt}
                 className="w-full h-full object-contain" />
-            ) : (<img src={preview} alt='Preview' className='w-9/12 h-9/12 object-contain opacity-40'/>)}
+            ) : (<img src={preview} alt='Preview' className='w-9/12 h-9/12 object-contain opacity-40' />)}
+
+            {generatingImg && (
+              <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                <Loader />
+              </div>
+            )}
           </div>
+        </div>
+        <div className='mt-5 flex gap-5'>
+          <button className='text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+            type='button' onClick={generateImage}>
+            {generatingImg ? 'Generating...' : 'Generate'}
+          </button>
+        </div>
+        <div className='mt-10'>
+          <p className='mt-2 text-[#666e75] text-[14px]'>Wanna share your image with community? </p>
+          
         </div>
       </form>
     </section>
