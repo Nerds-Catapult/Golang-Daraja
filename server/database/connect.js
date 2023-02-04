23ad35bd095bfe73e562f79bd4a96227f5c8a20f
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 12:42:39 +0300

    database

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true);
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-        });
+        const conn = await mongoose.connect(process.env.DATABASE_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     }
     catch (err) {
--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors'
-dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
 const PORT = process.env.PORT
+const connectDb = require('./database/connect');                                                                                                            
+
+
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+connectDb();
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-})
+app.listen(PORT, () => { console.log(`server started on port ${PORT}`)})
