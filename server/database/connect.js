b2660745dc176db56299360bd56a3dbc6d80dbfd
Domains18 <gibsonsgibson88@gmail.com>
2023-02-04 12:13:27 +0300

    backend

--- server/database/connect.js
+++ server/database/connect.js
@@ -1,9 +1,16 @@
 const mongoose = require('mongoose');
 
-const connectDB = (url) => {
-    mongoose.set('strictQuery', true);
-
-    mongoose.connect(url, {
-        
-    });
+const connectDb = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    }
+    catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
 }
+
+module.exports = connectDb
