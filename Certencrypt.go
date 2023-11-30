e635d14baf68e27c64ff849981197b81555a0d2b
Domains18 <gibsonsgibson88@gmail.com>
2023-11-30 08:36:04 +0300

    OpenSSl code refactor

--- Certencrypt.go
+++ Certencrypt.go
@@ -11,34 +11,39 @@ import (
 	"os"
 )
 
-func openSSlEncrypt(data, certPath string) (string, error){
-	certFile, err := os.Open(certPath)
-	if err != nil {
-		return "", err
-	}
-	defer func(certFile *os.File) {
-		err := certFile.Close()
-		if err != nil {
-
-		}
-	}(certFile)
-
-	certBytes, err := io.ReadAll(certFile)
+func openSSlEncrypt(data, certPath string) (string, error) {
+	cert, err := loadCertificate(certPath)
 	if err != nil {
 		return "", err
 	}
 
-	block, _ := pem.Decode(certBytes)
-	if block == nil {
-		return "", errors.New("failed to parse certificate PEM")
-	}
-	cert, err := x509.ParseCertificate(block.Bytes)
-	if err != nil {
-		return "", err
-	}
 	encrypted, err := rsa.EncryptPKCS1v15(rand.Reader, cert.PublicKey.(*rsa.PublicKey), []byte(data))
 	if err != nil {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(encrypted), nil
+}
+
+func loadCertificate(certPath string) (*x509.Certificate, error) {
+	certFile, err := os.Open(certPath)
+	if err != nil {
+		return nil, err
+	}
+	defer certFile.Close()
+
+	certBytes, err := io.ReadAll(certFile)
+	if err != nil {
+		return nil, err
+	}
+
+	block, _ := pem.Decode(certBytes)
+	if block == nil {
+		return nil, errors.New("failed to parse certificate PEM")
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return cert, nil
 }
